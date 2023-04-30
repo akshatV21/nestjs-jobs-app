d0b9748a05b195eeafa5a9c9bdebc9f56de2cbc3
Akshat Vishwakarma <108259385+akshatV21@users.noreply.github.com>
2023-04-30 15:19:17 +0000

    modified auth guard

--- libs/common/src/auth/guards/authorize.guard.ts
+++ libs/common/src/auth/guards/authorize.guard.ts
@@ -7,7 +7,7 @@ import {
   UnauthorizedException,
 } from '@nestjs/common'
 import { Reflector } from '@nestjs/core'
-import { ClientProxy } from '@nestjs/microservices'
+import { ClientProxy, RpcException } from '@nestjs/microservices'
 import { Observable, catchError, map, of, tap } from 'rxjs'
 import { SERVICES } from 'utils/constants'
 import { AuthOptions } from 'utils/interfaces'
@@ -20,6 +20,13 @@ export class Authorize implements CanActivate {
   ) {}
 
   canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+    const requestType = context.getType()
+
+    if (requestType === 'http') return this.authorizeHttpRequest(context)
+    else if (requestType === 'rpc') return this.authorizeRpcRequest(context)
+  }
+
+  private authorizeHttpRequest(context: ExecutionContext) {
     const { isOpen, isLive } = this.reflector.get<AuthOptions>('authOptions', context.getHandler())
 
     if (!isLive) throw new InternalServerErrorException('This endpoint is currently under mainatainence.')
@@ -31,18 +38,30 @@ export class Authorize implements CanActivate {
     if (!authHeader) throw new UnauthorizedException('Please log in first.')
 
     const token = authHeader.split(' ')[1]
+    return this.sendAuthorizeMessage(token, request)
+  }
 
+  private authorizeRpcRequest(context: ExecutionContext) {
+    const { isOpen, isLive } = this.reflector.get<AuthOptions>('authOptions', context.getHandler())
+
+    if (!isLive) throw new RpcException('This endpoint is currently under mainatainence.')
+    if (isOpen) return true
+
+    const request = context.switchToRpc().getData()
+
+    const token = request.token
+    return this.sendAuthorizeMessage(token, request)
+  }
+
+  private sendAuthorizeMessage(token: string, request: any) {
     return this.AuthClient.send('authorize', { token }).pipe(
       tap(res => {
-        console.log(`RESPONSE - `, res)
         const target = res.target
         request[target] = res[target]
+        request['token'] = token
       }),
       map(() => true),
-      catchError(err => {
-        console.log('!---------guard error---------!', err)
-        return of(false)
-      }),
+      catchError(err => of(false)),
     )
   }
 }
